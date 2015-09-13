ef81abc01b608d0baf8b1cc38ae17fe395994d22
cutefrank <littlescore@gmail.com>
2015-09-13 16:50:39 +0800

    First Try

---
+++ SchoolProject/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace SchoolProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFillComboBox = new System.Windows.Forms.Button();
+            this.cmbPersons = new System.Windows.Forms.ComboBox();
+            this.txtNewLastName = new System.Windows.Forms.TextBox();
+            this.txtNewFirstName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnInsert);
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Controls.Add(this.txtFirstName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insert";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(76, 99);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert Person";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(60, 52);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(60, 23);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LastName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FirstName";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnFillComboBox);
+            this.groupBox2.Controls.Add(this.cmbPersons);
+            this.groupBox2.Controls.Add(this.txtNewLastName);
+            this.groupBox2.Controls.Add(this.txtNewFirstName);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(21, 155);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(306, 162);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select,Update,Delete";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(60, 130);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(114, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update person";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(9, 44);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(119, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete person";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFillComboBox
+            // 
+            this.btnFillComboBox.Location = new System.Drawing.Point(144, 22);
+            this.btnFillComboBox.Name = "btnFillComboBox";
+            this.btnFillComboBox.Size = new System.Drawing.Size(132, 23);
+            this.btnFillComboBox.TabIndex = 5;
+            this.btnFillComboBox.Text = "Fill ComboBox";
+            this.btnFillComboBox.UseVisualStyleBackColor = true;
+            this.btnFillComboBox.Click += new System.EventHandler(this.btnFillComboBox_Click);
+            // 
+            // cmbPersons
+            // 
+            this.cmbPersons.FormattingEnabled = true;
+            this.cmbPersons.Location = new System.Drawing.Point(7, 22);
+            this.cmbPersons.Name = "cmbPersons";
+            this.cmbPersons.Size = new System.Drawing.Size(121, 20);
+            this.cmbPersons.TabIndex = 4;
+            // 
+            // txtNewLastName
+            // 
+            this.txtNewLastName.Location = new System.Drawing.Point(92, 102);
+            this.txtNewLastName.Name = "txtNewLastName";
+            this.txtNewLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtNewLastName.TabIndex = 3;
+            // 
+            // txtNewFirstName
+            // 
+            this.txtNewFirstName.Location = new System.Drawing.Point(92, 73);
+            this.txtNewFirstName.Name = "txtNewFirstName";
+            this.txtNewFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtNewFirstName.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "New Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "New First Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 328);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "專題查詢系統";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFillComboBox;
+        private System.Windows.Forms.ComboBox cmbPersons;
+        private System.Windows.Forms.TextBox txtNewLastName;
+        private System.Windows.Forms.TextBox txtNewFirstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ Session/Broker.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using System.Data;
+using System.IO;
+using System.Reflection;
+
+namespace Session
+{
+    public class Broker
+    {
+        OleDbConnection connection;
+        OleDbCommand command;
+
+        private void ConnectTo()
+        {
+            //string str = System.Environment.CurrentDirectory; 
+            //string filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/Database.accdb");
+            //connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=" + str + "\\Database.accdb");
+            //connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=MapPath(""Database.accdb"")");
+            //connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\user\Documents\Visual Studio 2013\Projects\SchoolProject\Database.accdb");
+            command = connection.CreateCommand();
+        }
+      
+        public Broker()
+        {
+            ConnectTo();
+        }
+
+        public void Insert(Person p)
+        {
+            try
+            {
+                command.CommandText = "INSERT INTO TPersons (FirstName,LastName) VALUES('" + p.FirstName + "', '" + p.LastName+"')";
+                command.CommandType = CommandType.Text;
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            finally
+            {
+                if(connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public List<Person> FillComboBox()
+        {
+            List<Person> personsList = new List<Person>();
+            try
+            {
+                command.CommandText = "SELECT * FROM TPersons";
+                command.CommandType = CommandType.Text;
+                connection.Open();
+
+                OleDbDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Person p = new Person();
+
+                    p.Id = Convert.ToInt32(reader["ID"].ToString());
+                    p.FirstName = reader["FirstName"].ToString();
+                    p.LastName = reader["LastName"].ToString();
+
+                    personsList.Add(p);
+                }
+                return personsList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public void Update(Person oldPerson,Person newPerson)
+        {
+            try
+            {                     
+                command.CommandText = "UPDATE TPersons  SET FirstName= '" + newPerson.FirstName + "',LastName = '" + newPerson.LastName +"' WHERE ID=" + oldPerson.Id ;
+                command.CommandType = CommandType.Text;
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public void Delete(Person p)
+        {
+            try
+            {
+                command.CommandText = "DELETE FROM TPersons WHERE ID= " + p.Id;
+                command.CommandType = CommandType.Text;
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
